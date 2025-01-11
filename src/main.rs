0c087511ed762f18140cab286c6773ae9881e5d7
Brewster Malevich <sbmalev@gmail.com>
2025-01-10 20:39:16 -0800

    Add web server logging, tracing

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use axum::{
 use deadpool_diesel::postgres::Pool;
 use dotenvy::dotenv;
 use std::env;
+use tower_http::trace::TraceLayer;
+use tracing::{debug, error, info};
 
 #[derive(Clone)]
 struct AppState {
@@ -21,6 +23,7 @@ struct AppState {
 async fn main() {
     // Init tracing
     tracing_subscriber::fmt::init();
+    info!("starting server");
 
     // Read in settings.
     dotenv().ok();
@@ -33,6 +36,7 @@ async fn main() {
     let conn_pool =
         create_database_connection_pool(database_url, 5).expect("unable to connect to database");
     // Here is where we'd do automated migrations if we're doing that.
+    debug!("setup database connection pool");
 
     let state = AppState { conn_pool };
 
@@ -41,8 +45,11 @@ async fn main() {
         .route("/facilities/{uid}", get(get_facility))
         .route("/facilities/", get(get_facilities))
         .route("/facilities/{uid}", delete(delete_facility))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
+    debug!("setup app routes");
 
+    info!("listening on {server_url:?}");
     let listener = tokio::net::TcpListener::bind(server_url).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
@@ -52,10 +59,15 @@ async fn post_facility(
     State(state): State<AppState>,
     Json(payload): Json<core::Facility>,
 ) -> Result<Json<core::Facility>, StatusCode> {
+    debug!("received request to post {payload:?}");
+
     let client_result = state.conn_pool.get().await;
     let client = match client_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error using the connection pool
+        Err(e) => {
+            error!("error collecting client from connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     let interaction_result = client
@@ -63,7 +75,10 @@ async fn post_facility(
         .await;
     let new_facility_result = match interaction_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error interacting with the database.
+        Err(e) => {
+            error!("error interacting through connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     match new_facility_result {
@@ -71,8 +86,11 @@ async fn post_facility(
         Err(diesel::result::Error::DatabaseError(
             diesel::result::DatabaseErrorKind::UniqueViolation,
             _,
-        )) => Err(StatusCode::CONFLICT),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR), // Log this. Not sure should use this status code.
+        )) => Err(StatusCode::CONFLICT), // TODO: Should we log this?
+        Err(e) => {
+            error!("error writing new facility to database {e:?}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
     }
 }
 
@@ -81,10 +99,15 @@ async fn get_facility(
     Path(uid): Path<String>,
     State(state): State<AppState>,
 ) -> Result<Json<core::Facility>, StatusCode> {
+    debug!("received request to get facility {uid:?}");
+
     let client_result = state.conn_pool.get().await;
     let client = match client_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error using the connection pool
+        Err(e) => {
+            error!("error collecting client from connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     let interaction_result = client
@@ -92,13 +115,19 @@ async fn get_facility(
         .await;
     let read_facility_result = match interaction_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log the interaction error? Why do these happen?
+        Err(e) => {
+            error!("error interacting through connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     match read_facility_result {
         Ok(matching_facility) => Ok(Json(matching_facility)),
         Err(diesel::result::Error::NotFound) => Err(StatusCode::NOT_FOUND),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Is this the only possible error we could have?
+        Err(e) => {
+            error!("error getting facility from database {e:?}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
     }
 }
 
@@ -107,10 +136,17 @@ async fn get_facilities(
     State(state): State<AppState>,
     Query(params): Query<FacilitiesFilter>,
 ) -> Result<Json<Vec<core::Facility>>, StatusCode> {
+    debug!("Received request to get facilities with filter {params:?}");
+
     let client_result = state.conn_pool.get().await;
     let client = match client_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error using the connection pool
+        Err(e) => {
+            return {
+                error!("error collecting client from connection pool {e:?}");
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        }
     };
 
     let interaction_result = client
@@ -118,12 +154,18 @@ async fn get_facilities(
         .await;
     let list_facilities_result = match interaction_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log the interaction error? Why do these happen?
+        Err(e) => {
+            error!("error interacting through connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     match list_facilities_result {
         Ok(facilities) => Ok(Json(facilities)),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error interacting with the database.
+        Err(e) => {
+            error!("error listing facilities from database {e:?}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
     }
 }
 
@@ -132,10 +174,15 @@ async fn delete_facility(
     State(state): State<AppState>,
     Path(uid): Path<String>,
 ) -> Result<StatusCode, StatusCode> {
+    debug!("Received request to delete facility {uid:?}");
+
     let client_result = state.conn_pool.get().await;
     let client = match client_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Error using the connection pool
+        Err(e) => {
+            error!("error collecting client from connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     let interaction_result = client
@@ -143,12 +190,20 @@ async fn delete_facility(
         .await;
     let delete_result = match interaction_result {
         Ok(r) => r,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log the interaction error? Why do these happen?
+        Err(e) => {
+            error!("error interacting through connection pool {e:?}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
     };
 
     match delete_result {
         Ok(_) => Ok(StatusCode::NO_CONTENT),
         Err(diesel::result::Error::NotFound) => Err(StatusCode::NOT_FOUND),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR), // TODO: Log this. Is this the only possible error we could have?
+        Err(e) => {
+            error!("error deleting facility from database {e:?}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
     }
 }
+
+// TODO: Format error messages properly.
